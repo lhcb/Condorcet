f31e060461036943264079a4588b65d6c22eaed5
Alex Pearce <alex@alexpearce.me>
2015-03-17 13:58:20 +0100

    Allow clicking <td> to act as click on radio.

--- Condorcet/static/js/main.js
+++ Condorcet/static/js/main.js
@@ -1,11 +1,20 @@
 $(function() {
   'use strict';
-  $('input:radio').on('change', function(e) {
-    var $radio = $(e.target);
+
+  // Do not allow multiple radio buttons to be checked in a given column
+  // The same condition for rows is automatically satisfied by the browser
+  // Uses special data-column attribute on each <td> to find cells in the same
+  // column
+  // This handler also always clicking anywhere within the <td> to act as a
+  // click on the contained radio input
+  $('.poll td').on('click', function(e) {
+    if ($(e.target).is('input:radio')) {
+        var $radio = $(e.target);
+    } else {
+        var $radio = $(e.target).children('input:radio');
+    }
     var colNum = $radio.parent('td').data('column');
-    $('td[data-column=' + colNum + ']').each(function() {
-      $(this).children('input:radio').prop('checked', false);
-    });
+    $('td[data-column=' + colNum + '] input:radio').prop('checked', false);
     $radio.prop('checked', true);
   });
 });
