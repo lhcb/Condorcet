de0f691741321bd4ff3779f1d7fa3c84e5af660c
Giulio Dujany <giulio.dujany@yahoo.it>
2015-03-18 18:12:09 +0100

    Change first line in cgi-bin/condorcet.fcgi

--- setup.sh
+++ setup.sh
@@ -9,3 +9,10 @@ chmod 755 Condorcet/manageDB.py
 Condorcet/manageDB.py --init
 fs setacl -dir Condorcet/databases -acl webserver:afs diklwr
 chmod +x cgi-bin/condorcet.fcgi
+
+# Change first line in cgi-bin/condorcet.fcgi
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+echo $DIR
+cat cgi-bin/condorcet.fcgi | sed -e "s;/afs/cern.ch/user/g/gdujany/www/Condorcet;$DIR;" > cgi-bin/tmp.fcgi
+mv cgi-bin/tmp.fcgi cgi-bin/condorcet.fcgi
+
