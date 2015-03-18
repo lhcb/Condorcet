d3eddacd537b6377f90681f9f0cc1f34feffdbfb
Giulio Dujany <giulio.dujany@yahoo.it>
2015-03-18 11:58:33 +0100

    Added setup.sh which should do the most of the job

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PATH=/usr/bin/python:$PATH
+virtualenv venv
+. venv/bin/activate
+pip install -r requirements.txt
+wget https://pypi.python.org/packages/2.6/f/flup/flup-1.0.2-py2.6.egg
+chmod 755 Condorcet/managDB.py
+Condorcet/manageDB.py --init
+fs setacl -dir Condorcet/databases -acl webserver:afs diklwr
+
