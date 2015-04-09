f4e7a8700790200f295bdb49cea9d5bb00456306
Giulio Dujany <giulio.dujany@yahoo.it>
2015-04-09 17:06:44 +0200

    Write modifiables options in database

--- Condorcet/verifyAuthors.py
+++ Condorcet/verifyAuthors.py
@@ -2,8 +2,9 @@ import xml.etree.ElementTree
 
 from config import AUTHORS_LIST
 
+default_authors_file = AUTHORS_LIST
 
-def listAuthors(authors_file=AUTHORS_LIST):
+def listAuthors(authors_file=default_authors_file):
     """Return the list of authors as full names.
 
     Authors are in root[4], the given name is root[4][i][0].text and surname
@@ -14,5 +15,5 @@ def listAuthors(authors_file=AUTHORS_LIST):
     return [' '.join([child[0].text, child[1].text]) for child in root[4]]
 
 
-def isAuthor(fullname, authors_file=AUTHORS_LIST):
+def isAuthor(fullname, authors_file=default_authors_file):
     return fullname in listAuthors(authors_file=authors_file)
