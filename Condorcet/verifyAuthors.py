21fb90effc80404265cb2373b7096ed12fc48d94
Giulio Dujany <giulio.dujany@yahoo.it>
2015-05-03 19:25:55 +0200

    customized fuzzy logic in comparing names

--- Condorcet/verifyAuthors.py
+++ Condorcet/verifyAuthors.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree
 import os
 from Condorcet import app
 from Condorcet.updateConfig import getConfig
-from fuzzywuzzy import process
+import unicodedata
 
 
 def default_authors_file():
@@ -24,6 +24,15 @@ def listAuthors(authors_file=default_authors_file()):
     return [' '.join([child[0].text, child[1].text]) for child in root[4]]
 
 
+def str_normalize(string):
+    norm1 = string.strip().lower().replace('-', ' ')
+    try:
+        return ''.join((c for c in unicodedata.normalize('NFD', norm1)
+                        if unicodedata.category(c) != 'Mn'))
+    except TypeError:
+        return norm1
+
+
 def isAuthor(fullname, authors_file=None):
     """
     Match using fuzzy logic and return fullname as appear in the
@@ -31,10 +40,18 @@ def isAuthor(fullname, authors_file=None):
     """
     if authors_file is None:
         authors_file = default_authors_file()
-    matches = process.extract(fullname, listAuthors(authors_file=authors_file),
-                              limit=3)
-    if (matches[0][1] > 80 and
-       (matches[0][1] - matches[1][1]) > (matches[1][1] - matches[2][1])):
-        return matches[0][0]
+
+    list_authors = listAuthors(authors_file=authors_file)
+    list_authors_norm = [str_normalize(i) for i in list_authors]
+    fullname_norm = str_normalize(fullname)
+
+    if fullname_norm in list_authors_norm:
+        return fullname
     else:
-        return False
+        parts = set(fullname_norm.split())
+        for name, name_norm in zip(list_authors, list_authors_norm):
+            if (set(name_norm.split()).issubset(parts) or
+               parts.issubset(set(name_norm.split()))):
+                return name
+
+    return False
