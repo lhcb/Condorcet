1fe8190e42c6e610ae82049d742c0f52a47223b9
Giulio Dujany <giulio.dujany@yahoo.it>
2015-05-08 22:16:21 +0200

    adding also other needed files

---
+++ Condorcet/verifyVoters.py
@@ -0,0 +1,22 @@
+import os
+from Condorcet import app
+from Condorcet.updateConfig import getConfig
+
+
+def default_voters_file():
+    return os.path.join(app.config['DB_DIR'], getConfig('VOTERS_LIST'))
+
+
+def listVoters(voters_file=default_voters_file()):
+    """
+    Return the list of cernid of voters
+    """
+    voters = [i.rstrip().split(',')[0]
+              for i in open(voters_file).readlines()[1:]]
+    return voters
+
+
+def isVoter(cernid, voters_file=None):
+    if voters_file is None:
+        voters_file = default_voters_file()
+    return cernid in listVoters(voters_file=voters_file)
