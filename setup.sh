c4580f168a4375a418c0f090c8465e2624c91a04
gdujany <giulio.dujany@cern.ch>
2017-05-08 13:22:59 +0200

    tell cernid of person unable to vote

--- setup.sh
+++ setup.sh
@@ -16,7 +16,12 @@ chmod +x cgi-bin/condorcet.fcgi
 # Change first line in cgi-bin/condorcet.fcgi
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo $DIR
+echo $(basename $DIR)
 cat cgi-bin/condorcet.fcgi | sed -e "s;/afs/cern.ch/user/g/gdujany/www/Condorcet;$DIR;" > cgi-bin/tmp.fcgi
 mv cgi-bin/tmp.fcgi cgi-bin/condorcet.fcgi
 chmod 755 cgi-bin/condorcet.fcgi
+
+# Change  '/lhcb-condorcet/Condorcet' to the right directory in Condorcet/config.py
+cat Condorcet/config.py | sed -e "s;/lhcb-condorcet/Condorcet;/lhcb-condorcet/$(basename $DIR);" > Condorcet/tmp.py
+mv Condorcet/tmp.py Condorcet/config.py
 
